91424b6009a08af5230c06a3b230b66c2e48be46
Alvaro Herrera <alvherre@alvh.no-ip.org>
2017-03-20 18:58:49 -0300

    remove empty line

--- src/backend/statistics/stat_ext.c
+++ src/backend/statistics/stat_ext.c
@@ -7,7 +7,6 @@
  * Portions Copyright (c) 1996-2017, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- *
  * IDENTIFICATION
  *	  src/backend/statistics/stat_ext.c
  *
