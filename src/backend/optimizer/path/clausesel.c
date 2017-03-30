an-tao/postgres

commit 232990d48a85fac9d84043069fd849171d3463ae

Add missing function prototypes
Two of these seemed to be missing, but instead of just adding the missing
ones I've copied and pasted all of the new static functions and pasted as
prototypes in the same order they're defined in the file.
mv_compatible_walker was left out due to the struct argument being defined
later in the file.

src/backend/optimizer/path/clausesel.c
@@ -46,17 +46,22 @@ typedef struct RangeQueryClause
 
 static void addRangeClause(RangeQueryClause **rqlist, Node *clause,
 			   bool varonleft, bool isLTsel, Selectivity s2);
-static bool clause_is_ext_compatible(Node *clause, Index relid, AttrNumber *attnum);
+static MVDependency *find_strongest_dependency(StatisticExtInfo *stats,
+											   MVDependencies *dependencies,
+											   Bitmapset *attnums);
+static Selectivity clauselist_ext_selectivity_deps(PlannerInfo *root, Index relid,
+								List *clauses, StatisticExtInfo *stats,
+								Index varRelid, JoinType jointype,
+								SpecialJoinInfo *sjinfo,
+								RelOptInfo *rel);
 static Bitmapset *collect_ext_attnums(List *clauses, Index relid);
-static StatisticExtInfo *choose_ext_statistics(List *stats,
-									Bitmapset *attnums, char requiredkind);
+static int count_attnums_covered_by_stats(StatisticExtInfo *info, Bitmapset *attnums);
+static StatisticExtInfo *choose_ext_statistics(List *stats, Bitmapset *attnums, char requiredkind);
 static List *clauselist_ext_split(PlannerInfo *root, Index relid,
 					List *clauses, List **mvclauses,
 					StatisticExtInfo *stats);
-static Selectivity clauselist_ext_selectivity_deps(PlannerInfo *root,
-						Index relid, List *clauses, StatisticExtInfo *stats,
-						Index varRelid, JoinType jointype,
-						SpecialJoinInfo *sjinfo, RelOptInfo *rel);
+//static bool mv_compatible_walker(Node *node, mv_compatible_context *context);
+static bool clause_is_ext_compatible(Node *clause, Index relid, AttrNumber *attnum);
 static bool has_stats_of_kind(List *stats, char requiredkind);
 
 

commit b37826c832eef020427d5c0d7ff6b17fe7695f94

Remove commented out function prototype.
Accidentally caused in 232990d48

src/backend/optimizer/path/clausesel.c
@@ -60,7 +60,6 @@ static StatisticExtInfo *choose_ext_statistics(List *stats, Bitmapset *attnums, 
 static List *clauselist_ext_split(PlannerInfo *root, Index relid,
 					List *clauses, List **mvclauses,
 					StatisticExtInfo *stats);
-//static bool mv_compatible_walker(Node *node, mv_compatible_context *context);
 static bool clause_is_ext_compatible(Node *clause, Index relid, AttrNumber *attnum);
 static bool has_stats_of_kind(List *stats, char requiredkind);
 